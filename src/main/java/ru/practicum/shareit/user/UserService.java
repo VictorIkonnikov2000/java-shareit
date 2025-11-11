b71c68d76d5238341c257cb5bf961286825c23d7
Виктор <vitya.tigr15@mail.ru>
2025-11-11 03:09:13 +0300

    ФЗ14 Исправленный чекстайл2

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.user.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+
     UserDto createUser(UserDto userDto);
+
     UserDto updateUser(Long userId, UserDto userDto);
+
     UserDto getUser(Long userId);
+
     List<UserDto> getAllUsers();
+
     void deleteUser(Long userId);
 }
