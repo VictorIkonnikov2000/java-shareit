d48877a9aa298c2cdcc4c4113a0afe1883090d08
Виктор <vitya.tigr15@mail.ru>
2025-12-05 20:08:15 +0300

    ФЗ16 чекстайл4

--- gateway/src/main/java/ru/practicum/shareit/gateway/config/RestTemplateConfig.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/config/RestTemplateConfig.java
@@ -1,16 +1,18 @@
-package ru.practicum.shareit.gateway.config;
+package ru.practicum.shareit.gateway.config; // Или любой другой подходящий пакет
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .build();
-    }
+public class RestTemplateConfig {@Bean
+public RestTemplate restTemplate() {
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+    requestFactory.setHttpClient(httpClient);
+    return new RestTemplate(requestFactory);
+}
 }
