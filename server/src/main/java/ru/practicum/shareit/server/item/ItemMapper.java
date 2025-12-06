198ba3a18602fb5a8067f3b02631f8fcd3fe805f
Виктор <vitya.tigr15@mail.ru>
2025-12-06 15:59:49 +0300

    ФЗ16 чекстайл13

--- server/src/main/java/ru/practicum/shareit/server/item/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/server/item/ItemMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.server.item;
 
-
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.server.item.dto.ItemDto;
 import ru.practicum.shareit.server.booking.dto.BookingShortDto;
@@ -16,8 +15,18 @@ public class ItemMapper {
     public ItemDto toItemDto(Item item) {
         ItemDto itemDto = new ItemDto();
         itemDto.setId(item.getId());
-        itemDto.setName(item.getName());
-        itemDto.setDescription(item.getDescription());
+        // ***ВАЖНОЕ ИЗМЕНЕНИЕ ЗДЕСЬ***
+        if (item.getName() != null) {
+            itemDto.setName(item.getName());
+        } else {
+            itemDto.setName("Unknown Item Name"); // Заменяем null на строку по умолчанию
+        }
+        // ***ВАЖНОЕ ИЗМЕНЕНИЕ ЗДЕСЬ***
+        if (item.getDescription() != null) {
+            itemDto.setDescription(item.getDescription());
+        } else {
+            itemDto.setDescription("No description provided"); // Заменяем null на строку по умолчанию
+        }
         itemDto.setAvailable(item.getAvailable());
         itemDto.setUserId(item.getOwnerId());
         if (item.getRequest() != null) {
@@ -27,6 +36,7 @@ public class ItemMapper {
     }
 
     public ItemDto toItemDtoWithBookings(Item item, BookingShortDto lastBooking, BookingShortDto nextBooking) {
+        // Переиспользуем логику toItemDto, чтобы изменения применились автоматически
         ItemDto itemDto = toItemDto(item);
         itemDto.setLastBooking(lastBooking);
         itemDto.setNextBooking(nextBooking);
@@ -34,6 +44,7 @@ public class ItemMapper {
     }
 
     public ItemDto toItemDtoWithBookingsAndComments(Item item, BookingShortDto lastBooking, BookingShortDto nextBooking, List<CommentDto> comments) {
+        // Переиспользуем логику toItemDto, чтобы изменения применились автоматически
         ItemDto itemDto = toItemDto(item);
         itemDto.setLastBooking(lastBooking);
         itemDto.setNextBooking(nextBooking);
@@ -44,8 +55,19 @@ public class ItemMapper {
     public static Item toItem(ItemDto itemDto, Long userId) {
         Item item = new Item();
         item.setId(itemDto.getId());
-        item.setName(itemDto.getName());
-        item.setDescription(itemDto.getDescription());
+        // ***Добавляем проверку на null в обратном маппинге, если itemDto.getName() может быть null***
+        if (itemDto.getName() != null) {
+            item.setName(itemDto.getName());
+        } else {
+            // Опционально: если name не может быть null в базе, возможно, нужно выбросить исключение
+            // или установить какое-то дефолтное "пустое" значение, в зависимости от бизнес-логики.
+            // item.setName("");
+        }
+        if (itemDto.getDescription() != null) {
+            item.setDescription(itemDto.getDescription());
+        } else {
+            // item.setDescription("");
+        }
         item.setAvailable(itemDto.getAvailable());
         item.setOwnerId(userId);
         return item;
@@ -54,16 +76,22 @@ public class ItemMapper {
     public static Item toItem(ItemDto itemDto) {
         Item item = new Item();
         item.setId(itemDto.getId());
-        item.setName(itemDto.getName());
-        item.setDescription(itemDto.getDescription());
+        // ***Добавляем проверку на null в обратном маппинге***
+        if (itemDto.getName() != null) {
+            item.setName(itemDto.getName());
+        }
+        if (itemDto.getDescription() != null) {
+            item.setDescription(itemDto.getDescription());
+        }
         item.setAvailable(itemDto.getAvailable());
         item.setOwnerId(itemDto.getUserId());
         return item;
     }
 
     public Item toItem(ItemDto itemDto, Long userId, ItemRequest itemRequest) {
-        Item item = toItem(itemDto, userId);
+        Item item = toItem(itemDto, userId); // Это вызовет наш модифицированный toItem
         item.setRequest(itemRequest);
         return item;
     }
 }
+
