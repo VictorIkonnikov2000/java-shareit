6f9e162699c6fbf0ad4b4384d7efce8ead082070
Виктор <vitya.tigr15@mail.ru>
2025-12-06 17:20:07 +0300

    ФЗ16 чекстайл19

--- server/src/main/java/ru/practicum/shareit/server/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/server/request/dto/ItemRequestDto.java
@@ -4,15 +4,23 @@ import lombok.Data;
 import ru.practicum.shareit.server.item.dto.ItemDto;
 import ru.practicum.shareit.server.user.dto.UserDto; // Импортируем UserDto
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Data
 public class ItemRequestDto {
     private Long id;
+    @NotBlank(message = "Описание запроса не может быть пустым")
+    @Size(max = 2048, message = "Описание запроса не может быть больше 2048 символов")
     private String description;
-    private UserDto requestor; // <--- Теперь это объект UserDto
-    private LocalDateTime created;
-    private List<ItemDto> items;
-}
 
+    // Вместо requestorId, добавляем UserDto
+    private UserDto requestor; // <--- Меняем на объект UserDto
+
+    private LocalDateTime created;
+    private List<ItemDto> items; // Список ItemDto
+
+}
