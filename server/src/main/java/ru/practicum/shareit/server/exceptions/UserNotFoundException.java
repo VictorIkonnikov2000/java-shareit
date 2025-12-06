e39a7278ac3f663990a29b99aaa8cb07c4e1af61
Виктор <vitya.tigr15@mail.ru>
2025-12-06 12:58:11 +0300

    ФЗ16 чекстайл7

--- server/src/main/java/ru/practicum/shareit/server/exceptions/UserNotFoundException.java
+++ server/src/main/java/ru/practicum/shareit/server/exceptions/UserNotFoundException.java
@@ -1,7 +1,10 @@
 package ru.practicum.shareit.server.exceptions;
 
-public class UserNotFoundException extends RuntimeException { // Или Exception, в зависимости от задачи
-    private Long userId;public UserNotFoundException(String message, Long userId) {
+public class UserNotFoundException extends RuntimeException {
+
+    private Long userId;
+
+    public UserNotFoundException(String message, Long userId) {
         super(message);
         this.userId = userId;
     }
