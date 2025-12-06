4c3f008ef7a2663433e18640a7b60b96117d6479
Виктор <vitya.tigr15@mail.ru>
2025-12-06 15:54:29 +0300

    ФЗ16 чекстайл12

--- server/src/main/java/ru/practicum/shareit/server/user/UserMapper.java
+++ server/src/main/java/ru/practicum/shareit/server/user/UserMapper.java
@@ -9,16 +9,30 @@ public class UserMapper {
     public User toUser(UserDto userDto) {
         User user = new User();
         user.setId(userDto.getId());
-        user.setName(userDto.getName());
+        // Добавляем проверку на null, если name может быть null в UserDto
+        if (userDto.getName() != null) {
+            user.setName(userDto.getName());
+        } else {
+            // Опционально: можно установить значение по умолчанию, если считаете нужным
+            // user.setName("Default Name");
+        }
         user.setEmail(userDto.getEmail());
         return user;
     }
 
-    public UserDto toUserDto(User user) { // <--- УБРАНО "static"
+    public UserDto toUserDto(User user) {
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
-        userDto.setName(user.getName());
+        // ***ВАЖНОЕ ИЗМЕНЕНИЕ ЗДЕСЬ***
+        if (user.getName() != null) {
+            userDto.setName(user.getName());
+        } else {
+            // Если name извлекается из базы как null, присваиваем ему какое-то значение по умолчанию
+            // чтобы избежать null в JSON, который может вызвать TypeError в JS-тесте.
+            userDto.setName("Unknown User"); // <--- Вот здесь меняем null на строку
+        }
         userDto.setEmail(user.getEmail());
         return userDto;
     }
 }
+
