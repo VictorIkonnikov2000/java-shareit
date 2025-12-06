b2b9253a18a455c9f9b895fd26c99a6159f12ef2
Виктор <vitya.tigr15@mail.ru>
2025-12-06 21:50:17 +0300

    ФЗ16 чек6

--- server/src/main/java/ru/practicum/shareit/server/item/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/server/item/ItemRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.server.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             "OR LOWER(i.description) LIKE LOWER(concat('%', :text, '%')))")
     List<Item> searchAvailableItems(String text);
 
-    List<Item> findByRequest(Long requestId);
+    @Query("SELECT i FROM Item i WHERE i.request.id = :requestId")
+    List<Item> findByRequest(@Param("requestId") Long requestId);
 }
 
